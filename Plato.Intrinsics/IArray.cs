c103e5d3c50e46f73d711ac5132fd6221eb550be
Christopher Diggins <cdiggins@gmail.com>
2025-05-16 12:08:38 -0400

    Code moved to Ara3D-SDK

--- Plato.Intrinsics/IArray.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Plato
-{
-    public interface IArray<T> : IReadOnlyList<T>
-    {
-        Integer Count { get; }
-        T At(Integer n); 
-        T this[Integer n] { get; }
-    }
-}
